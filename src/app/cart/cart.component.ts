faaf5b8db8bda842faf1005976cbd1844305fea4
Giorgi <ineveraskedtobebornmom@gmail.com>
2019-07-05 20:48:45 +0400

    feat(9-reactive-forms-form-validation): Add some rudimentary validaiton

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -20,7 +20,7 @@ export class CartComponent implements OnInit {
         this.clear = this.cartService.clearCart;
 
         this.checkedForm = formBuilder.group({
-            name: '',
+            name: ['', Validators.minLength(2)],
             address: formBuilder.group({
                 street: '',
                 city: '',
