5d11b32ff09748378a0de505b36383d7e39d3e91
Giorgi <ineveraskedtobebornmom@gmail.com>
2019-07-05 20:36:32 +0400

    feat(8-reactive-forms-nested-forms): Add a pretty nested form

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -21,7 +21,12 @@ export class CartComponent implements OnInit {
 
         this.checkedForm = formBuilder.group({
             name: '',
-            address: '',
+            address: formBuilder.group({
+                street: '',
+                city: '',
+                state: '',
+                zip: '',
+            })
         });
     }
 
