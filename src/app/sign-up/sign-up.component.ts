2d78229e26383538c4b92e4e12834dc5a483b321
Giorgi <ineveraskedtobebornmom@gmail.com>
2019-07-08 20:26:12 +0400

    feat(forms-task-1): Finish implementing the functionality

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, Validators, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-sign-up',
@@ -10,16 +10,31 @@ export class SignUpComponent implements OnInit {
 
     checkedForm;
 
+    static isMatching(password1: string, password2: string) {
+        return password1 === password2;
+    }
+
     constructor(private formbuilder: FormBuilder) {
         this.checkedForm = formbuilder.group({
             email: ['', Validators.email],
-            password: ['', [Validators.minLength(8), this.validPassword()]]
+            password: ['', Validators.pattern(/^[a-zA-Z0-9]{7,}$/)],
+            password2: ['', Validators.pattern(/^[a-zA-Z0-9]{7,}$/)],
+            nickname: ['', Validators.pattern(/^[a-zA-Z0-9\-]*$/)],
+            phone: ['', Validators.pattern(/^\+380\d{9}$/)],
+            website: ['', Validators.pattern(/^(http[s]?:\/\/)?(www\.)?\w*\.\w*$/)],
+            terms: [false],
+        }, {
+            validators: this.crossValidation,
         });
     }
 
-    validPassword() {
-        return (formControl) => {
-            return formControl.value === 'Roman' ? {forbidden: {invalid: true}} : null;
+    crossValidation(formGroup) {
+        const pw1 = formGroup.get('password').value;
+        const pw2 = formGroup.get('password2').value;
+
+        return pw1 === pw2 ? null : {
+            pw1,
+            pw2
         };
     }
 
@@ -28,7 +43,6 @@ export class SignUpComponent implements OnInit {
         this.checkedForm.reset();
     }
 
-    ngOnInit() {
-    }
+    ngOnInit() { }
 
 }
