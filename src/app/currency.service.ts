ac02ced19409c38696e1b10fbc9bb9bdcefd355d
Giorgi <ineveraskedtobebornmom@gmail.com>
2019-07-09 20:01:26 +0400

    feat(17-observable-rxjs-part-3): Implement stuff as seen on the lecture

--- src/app/currency.service.ts
+++ src/app/currency.service.ts
@@ -1,50 +1,36 @@
 import { Injectable } from '@angular/core';
-import { data } from './rates';
-import { Observable } from 'rxjs';
+import { data } from './currencies';
+import { Observable, from } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrencyService {
-  result;
+  currencies;
   Observer;
 
-  constructor() {
-    this.result = this.transformObjectToArray(data.rates);
-
+  constructor(private httpClient: HttpClient) {
+    this.currencies = data;
     this.Observer = new Observable(this.subscribe());
-  }
-
-  transformObjectToArray(object) {
-    const result = [];
-    const keys = Object.keys(object);
-    for (const key of keys) {
-      const value = object[key];
-      const item = {
-        currency: key,
-        value,
-      };
-
-      result.push(item);
-    }
-    return result;
   }
 
   subscribe() {
     return (subscriber) => {
       let i = 0;
+      from(this.currencies).subscribe(currency => {
+        const url = `https://api.exchangeratesapi.io/latest?symbols=${currency}`;
+        this.httpClient
+        .get(url)
+        .subscribe(value => {
+          subscriber.next(value);
+          i++;
 
-      for (const item of this.result) {
-        setTimeout(() => {
-          subscriber.next(item);
-        }, i * 500);
-
-        i++;
-      }
-
-      setTimeout(() => {
-        subscriber.complete();
-      }, i * 500);
+          if (i === this.currencies.length) {
+            subscriber.complete();
+          }
+        });
+      });
     };
   }
 }
--- src/app/currency/currency.component.ts
+++ src/app/currency/currency.component.ts
@@ -24,15 +24,23 @@ export class CurrencyComponent implements OnInit {
     };
 
     const filterCallback = ({value}) => value > 2;
+    const mapCallback = ({rates}) => {
+      const currency = Object.keys(rates)[0];
+      const value = rates[currency];
 
-    const mapCallback = ({currency, value}) => ({currency, value, icon: '🏦'});
+      return {
+        currency,
+        value,
+        icon: '🏦',
+      };
+    };
 
     const Observer = this.currencyService.Observer;
 
     Observer
     .pipe(
-      filter(filterCallback),
-      map(mapCallback)
+      map(mapCallback),
+      filter(filterCallback)
     )
     .subscribe({
       next: action,
